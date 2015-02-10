9aeb9270893da3bf7e35d4639608c8c3d1b2dce6
Rajesh Sankaran <rajesh@anl.gov>
2015-02-10 18:56:12 +0000

    Progress. The current code base is stable and works with Bretts Version0.2_rc1 cloud components. Tested between waggle mobile and beehive mobile.

--- core/frameworks/common/waggle/device/node_controller/send_msg.py
+++ core/frameworks/common/waggle/device/node_controller/send_msg.py
@@ -4,12 +4,9 @@ import time
 
 
 data_client = NodeDataClient(*CLOUD_SERVER_INFO)
+print data_client
 # add try catch to try to establish connection if doesn't exist
 def send_msg(msg):
-    #print ("Ready to send msg:%0.4f"%time.time())
-#    print ("connection established:%0.4f"%time.time())
-    data_client.msg_send(msg)
-    #print ("Msg sent :%0.4f"%time.time())
-#    data_client.close()
-#    print ("Socket closed:%0.4f"%time.time())
+    print data_client.msg_send(msg)
 
+send_msg("Test")
