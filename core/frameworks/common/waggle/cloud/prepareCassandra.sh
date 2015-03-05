2d99fa19014da21afef9cd4de0b831c5221cb455
Rajesh Sankaran <r@anl.gov>
2015-03-05 11:28:52 -0600

    some more cleanup

--- core/frameworks/common/waggle/cloud/prepareCassandra.sh
+++
@@ -1,7 +0,0 @@
-sudo mkdir /var/lib/cassandra
-sudo mkdir /var/lib/cassandra/commitlog
-sudo mkdir /var/lib/cassandra/saved_caches
-sudo mkdir /var/log/cassandra
-sudo chmod -R 777 /var/lib/cassandra
-sudo chmod -R 777 /var/log/cassandra
-
