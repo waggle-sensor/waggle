35c17085ab9c0e2df21c4576b77a34f1c2d85096
Rajesh Sankaran <r@anl.gov>
2015-06-17 13:32:28 -0500

    Rookie mistake..

--- resources/wag_sensor_firmware/flash_init.sh
+++ resources/wag_sensor_firmware/flash_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-avrdude -C/root/ship/avrdude.conf -v -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:/root/ship/initialBoot.cpp.hex:i 
+avrdude -C./avrdude.conf -v -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:./initialBoot.cpp.hex:i 
 
 
 
