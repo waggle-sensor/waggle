ede2ffc1a442acf779d607ee7607c6d668edf056
Amy <awagoner@anl.gov>
2015-06-25 14:07:10 -0500

    Added make_data_packet and a sensor data send test for GN

--- GN/TestPacking.py
+++ GN/TestPacking.py
@@ -1,23 +1,16 @@
+import PacketHandler
 from PacketHandler import *
 header_dict = {
-    "prot_ver"         : "0.3",
-    "flags"            : (5,6,True),
-    "len_body"         : 42,
-    "time"             : 42,
-    "msg_mj_type"      : 42,
-    "msg_mi_type"      : 42,
-    "snd_session"      : 42,
-    "s_uniqid"         : 42,
-    "ext_header"       : 42,
-    "resp_session"     : 42,
-    "r_uniqid"         : 42,
-    "snd_seq"          : 42,
-    "resp_seq"         : 42
+    "msg_mj_type"      : ord('p'),
+    "msg_mi_type"      : ord('r'),
 }
 
 body = "This is a much longer sample message that might be sent to the RabbitMQ broker to be used for waggle stuff."
 
+print PacketHandler.SEQUENCE
 packet = pack(header_dict,body)
+print PacketHandler.SEQUENCE
+header_dict.update(get_header(packet))
 unpacked = unpack(packet)
 if (header_dict,body) == (unpacked[0],unpacked[1]):
     print "TestPacking succeeded"
--- GN/utilities/packetmaker.py
+++ GN/utilities/packetmaker.py
@@ -1,36 +1,54 @@
 # packetmaker.py
 """
-	This module contains a few utilities that autogenerate complete simple packets,
-	such as ping and time request packets.
+This module contains a few utilities that autogenerate complete simple packets,
+such as ping and time request packets.
 """
 
 import sys
 sys.path.append("..")
-from protocol.src.PacketHandler import *
+from GN.PacketHandler import *
+from gPickler import gPickler
 
 # TODO: Find a way around two separate sequence sequences when using this module
+#TODO probably need to add params recipient = , and flags = . Maybe flags can be stored in config file for each GNode 
 
 
 def make_ping_packet():
-	"""
-		Returns a simple ping request packet.
-
-		:rtype: string
-	"""
-	header_dict = {
-		"msg_mj_type" : ord('p'),
-		"msg_mi_type" : ord('r')
-	}
-	return pack(header_dict)
+    """
+    Returns a simple ping request packet.
+    
+    :rtype: string
+    """
+    header_dict = {
+        "msg_mj_type" : ord('p'),
+        "msg_mi_type" : ord('r')
+        }
+    return pack(header_dict)
 
 def make_time_packet():
-	"""
-		Returns a simple time request packet.
+    """
+    Returns a simple time request packet.
+    
+    :rtype: string
+    """
+    header_dict = {
+        "msg_mj_type" : ord('t'),
+        "msg_mi_type" : ord('r')
+        }
+    return pack(header_dict)
 
-		:rtype: string
-	"""
-	header_dict = {
-		"msg_mj_type" : ord('t'),
-		"msg_mi_type" : ord('r')
-	}
-	return pack(header_dict)
+#TODO Need to change to reflect how the GN will actually get, pack, and send data
+def make_data_packet(data):
+    """
+    Compresses sensor data and returns a sensor data packet. 
+    
+    :param data: sensor data 
+    :rtype: string 
+    """ 
+    msg = gPickler(data)
+    header_dict = {
+        "msg_mj_type" : ord('s'),
+        "msg_mi_type" : ord('d')
+        }
+    return pack(header_dict, message_data = msg)
+    
