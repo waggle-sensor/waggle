c8de7400746674edd64b84f15a072cc5cd11c893
Rajesh Sankaran <r@anl.gov>
2015-07-01 09:54:38 -0500

    Pulled in version0.2_stable, moved out Nikhats files, did not find an easy wayto track that history.

--- other/version0.2_stable/frameworks/node/genericnode/clean_dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm *.pyc
-rm *.log
-rm *.cfg
-rm session_ids
-rm sensor1c
--- other/version0.2_stable/frameworks/node/genericnode/run_gm.py
+++
@@ -1,9 +0,0 @@
-import time
-from gn_global_definition_section import logger
-from gn_main_class import main_class
-logger.critical("Starts:"+str('%0.4f' % time.time())+"\n\n")
-# port at which GN can contact NC
-nc_port = 7001
-# main_thread starts everything else
-main_thread = main_class("main_thread", nc_port)
-main_thread.run()
