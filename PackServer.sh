5164c63588ffd38bf09f61b7f31adb6eaf13bf1a
Ben Harsha <bharsha92@gmail.com>
2015-07-24 11:05:24 -0500

    Added script that packs the server in targz format.

---
+++ PackServer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script packs up the server in a nice easy-to-distribute .tar.gz file
+read -p "Enter the name of the server as you want it distributed: " dirName
+mkdir $dirName
+cp -r server $dirName/
+mkdir $dirName/devtools
+cp -r devtools/protocol_common $dirName/devtools
+chmod 777 -R $dirName
+tar -cvzf $dirName.tar.gz $dirName
+rm -rf $dirName
+exit 0
