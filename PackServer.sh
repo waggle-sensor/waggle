ef5e11645f2a0f48dbed8d05101bbe2b68687c12
Ben Harsha <bharsha92@gmail.com>
2015-07-24 14:32:29 -0500

    Configuration updates.

--- PackServer.sh
+++ PackServer.sh
@@ -6,6 +6,7 @@ mkdir $dirName
 cp -r server $dirName/
 mkdir $dirName/devtools
 cp -r devtools/protocol_common $dirName/devtools
+cp WaggleServer.sh $dirName/
 chmod 777 -R $dirName
 tar -cvzf $dirName.tar.gz $dirName
 rm -rf $dirName
