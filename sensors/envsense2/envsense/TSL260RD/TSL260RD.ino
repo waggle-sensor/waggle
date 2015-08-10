8685a777ce868db001d065dbefb32c5f5fa02f9d
Kyle Lueptow <kylelueptow@gmail.com>
2015-08-10 15:58:35 -0500

    code stubbs for testing individual sensoes

--- sensors/envsense2/envsense/TSL260RD/TSL260RD.ino
+++ sensors/envsense2/envsense/TSL260RD/TSL260RD.ino
@@ -1,12 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
-  pinMode(A1,OUTPUT);
+  pinMode(A2,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int x = analogRead(A1);
+  int x = analogRead(A2);
   Serial.println(x);
   delay(500);
 } 
