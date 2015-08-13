b8f6a1991bfaff0b783669e9383d42f31a12572c
Rajesh Sankaran <r@anl.gov>
2015-08-13 14:46:51 -0500

    Adding PR103J2

---
+++ sensors/sensorcluster/PR103J2/PR103J2.ino
@@ -0,0 +1,18 @@
+#define A2D_PRJ103J2 0
+int PRJ103J2 = 0;           // variable to store the value read
+
+void setup()
+{
+    delay(5000);
+    SerialUSB.begin(9600);          //  setup serial
+}
+
+
+
+void loop()
+
+{
+    PRJ103J2 = analogRead(A2D_PRJ103J2);
+    SerialUSB.println(PRJ103J2);
+    delay(1000);
+}
