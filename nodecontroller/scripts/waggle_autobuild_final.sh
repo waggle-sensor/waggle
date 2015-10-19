1e2349fce6d90022f459fca054a00f0359f19d87
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2015-10-19 16:02:25 -0500

    put old rc.local in place

--- nodecontroller/scripts/waggle_autobuild_final.sh
+++ nodecontroller/scripts/waggle_autobuild_final.sh
@@ -54,9 +54,31 @@ fi
 
 
 # extract the report.txt from the new waggle image
-mkdir -p /media/waggle/
-mount ${DEVICE}${DEV_SUFFIX}2 /media/waggle/
-cp /media/waggle/root/report.txt .
+export WAGGLE_ROOT="/media/waggle/"
+mkdir -p ${WAGGLE_ROOT}
+mount ${DEVICE}${DEV_SUFFIX}2 ${WAGGLE_ROOT}
+cp ${WAGGLE_ROOT}/root/report.txt .
+cp ${WAGGLE_ROOT}/root/rc.local.log ./waggle.rc.local.log
+
+# put original rc.local inplace again
+cat <<EOF > ${WAGGLE_ROOT}/etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+[ ! -f /etc/ssh/ssh_host_dsa_key ]; dpkg-reconfigure openssh-server
+
+exit 0
+EOF
 
 #get size
 export FILESYSTEM_SIZE_KB=`df -BK --output=used ${DEVICE}${DEV_SUFFIX}2 | grep -o "[0-9]\+"` ; echo "FILESYSTEM_SIZE_KB: ${FILESYSTEM_SIZE_KB}"
