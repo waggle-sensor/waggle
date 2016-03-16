5db4b2adb3a94d01908b55861eab2659037fcda4
Rajesh Sankaran <r@anl.gov>
2016-03-16 01:01:35 -0500

    Heartbeat test.

---
+++ wagman/v3/manager/qa/wagman_heartbeat_test/wagman/wagman.ino
@@ -0,0 +1,46 @@
+#include "config.cpp"
+
+#include <Wire.h>
+#include <avr/wdt.h>
+
+#include "./libs/MCP79412RTC/MCP79412RTC.h"    //http://github.com/JChristensen/MCP79412RTC
+#include "./libs/Time/Time.h"
+
+
+unsigned char WagID[8];
+
+int question_no = 1;
+
+void setup()
+{
+    delay(1000);
+    set_up_pinmodes();
+    delay(1000);
+    Serial.begin(115200);
+    Wire.begin();
+    delay(1000);
+}
+
+
+
+
+void loop()
+
+{
+    Serial.println("------Wagman board test------");
+    WagID_print ();
+    delay(2000);
+    Serial.println("");
+    Serial.print(question_no++); Serial.print(". ");
+    Serial.println("Heartbeat test:");
+    delay(2000);
+    Serial.println("Reading heartbeat values for PORT1, PORT2, PORT3, PORT4 and PORT5 every 500ms:");
+    Serial.println(" ");
+    while(1)
+    {
+        hbt_read();
+        delay(470);
+    }
+}
+
+
