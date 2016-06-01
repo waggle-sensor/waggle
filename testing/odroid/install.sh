3a3fa702c6425d8eccbdc98b732859b70dc5d00b
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-01 16:26:31 -0500

    installs upstart script

---
+++ testing/odroid/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+CURRENT_DIR=$(pwd)
+
+
+if [ ! -e ${CURRENT_DIR}/stress-test.sh ] ; then
+  echo "${CURRENT_DIR}/stress-test.sh not found"
+  echo "please run install.sh in the same directory where stress-test.sh is installed."
+  exit 1
+fi
+
+
+sed -e 's:\[location\]:'${CURRENT_DIR}'/:' stress-test.conf_tmpl > /etc/init/stress-test.conf
+
