81e5fb3b2ffd1780cc13997d52ab00569a371192
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-01 17:10:56 -0500

    integrate label printing stuff

--- testing/odroid/install.sh
+++ testing/odroid/install.sh
@@ -16,3 +16,18 @@ fi
 sed -e 's:\[location\]:'${CURRENT_DIR}'/:' stress-test.conf_tmpl > /etc/init/stress-test.conf
 
 ./install-stress-ng.sh
+
+
+# install label_printing
+
+git clone https://github.com/waggle-sensor/label_printing.git
+cd label_printing/
+./install.sh
+
+
+# reconfigure udev rules
+
+sed -e "s:\[% SCRIPT %\]:`pwd`/print_status.sh:" label_printing/75-Dymo-LabelWriter-450.rules_template > /etc/udev/rules.d/75-Dymo-LabelWriter-450.rules
+
+
+### TODO: write print_status.sh
