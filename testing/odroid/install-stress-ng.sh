2ecd5c21d1d9f69cb76e80390e515c0cd33ea5d9
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2016-06-02 16:36:23 -0500

    more robust

--- testing/odroid/install-stress-ng.sh
+++ testing/odroid/install-stress-ng.sh
@@ -7,8 +7,14 @@ VERSION="0.06.05"
 
 apt-get install -y libattr1-dev libkeyutils-dev libbsd-dev
 
-wget http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-${VERSION}.tar.gz
+if [ ! -e stress-ng-${VERSION}.tar.gz ] ; then
+  wget http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-${VERSION}.tar.gz
+fi
 tar xvzf stress-ng-${VERSION}.tar.gz
 cd stress-ng-${VERSION}
-make
+
+if [ ! -e stress-ng-${VERSION}/stress-ng ] ; then 
+  make
+fi
+
 cp stress-ng /usr/local/bin/
