c930486e1b4dd3b3878555bf34bd26110d1e685b
Seongha Park <park708@purdue.edu>
2016-06-20 18:34:09 -0500

    update

--- coresensors/v3/pbay/coresense/getData.py
+++ coresensors/v3/pbay/coresense/getData.py
@@ -3,7 +3,7 @@ import time
 
 while 1:
     try:
-	link = serial.Serial("/dev/waggle_due_program", 115200)
+	link = serial.Serial("/dev/waggle_coresense", 115200)
 	print "connected"
 	break
     except:
@@ -15,8 +15,8 @@ while 1:
     data = link.readline()
 #    print data[:3]
 #    data.index('\r')
-    print data
-#    print ":".join("{:02x}".format(ord(c)) for c in data)
+#    print data
+    print ":".join("{:02x}".format(ord(c)) for c in data)
 #    hex_data = data.decode('hex')
 #    print " "
 #    print hex_data
