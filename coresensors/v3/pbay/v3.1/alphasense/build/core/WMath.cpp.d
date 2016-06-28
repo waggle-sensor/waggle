29b37139b162adb6300e04340cda2df02a281afa
Seongha Park <park708@purdue.edu>
2016-06-28 17:41:16 -0500

    clean up

--- coresensors/v3/pbay/v3.1/alphasense/build/core/WMath.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/spark/waggle_sensors/waggle_june20/coresensors/v3/pbay/alphasense/build/core/WMath.cpp.o: \
- /home/spark/.arduino15/packages/arduino/hardware/sam/1.6.7/cores/arduino/WMath.cpp \
- /home/spark/.arduino15/packages/arduino/hardware/sam/1.6.7/cores/arduino/WMath.h
