4b95743abf95b449f4a4eb625482a221cf76896f
Sean Shahkarami <sean.shahkarami@gmail.com>
2016-06-29 23:07:35 -0500

    renamed msg to command

--- playground/wagman-zmq/wagman-server.py
+++ playground/wagman-zmq/wagman-server.py
@@ -11,14 +11,11 @@ context = zmq.Context()
 # Subscribes to Wagman publisher to get outgoing message.
 sub = context.socket(zmq.SUB)
 sub.connect('tcp://localhost:5555')
-
-# Give the subscriber a chance to connect.
 time.sleep(1)
 
 # Connects to Wagman command pull to which it feeds commands from req.
 push = context.socket(zmq.PUSH)
 push.connect('tcp://localhost:5554')
-
 time.sleep(1)
 
 # Listens for incoming req / rep sequences.
@@ -27,12 +24,11 @@ rep.bind('tcp://*:5556')
 
 
 while True:
-    msg = rep.recv()
+    command = rep.recv()
 
-    # should do this with a timeout
-    sub.setsockopt(zmq.SUBSCRIBE, msg)
-    push.send(msg)
+    sub.setsockopt(zmq.SUBSCRIBE, command)
+    push.send(command)
     output = sub.recv()
-    sub.setsockopt(zmq.UNSUBSCRIBE, msg)
+    sub.setsockopt(zmq.UNSUBSCRIBE, command)
 
     rep.send(output)
